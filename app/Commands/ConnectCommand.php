00c6af5f1f9b01ec0f2e33ba851a058052328f07
Abul Hassan <imabulhasan99@gmail.com>
2026-01-03 17:34:52 +0600

    Enhance ConnectCommand to support WebSocket connection with error handling

--- app/Commands/ConnectCommand.php
+++ app/Commands/ConnectCommand.php
@@ -3,15 +3,44 @@
 namespace App\Commands;
 
 use Illuminate\Console\Command;
+use Ratchet\Client\WebSocket;
+use React\EventLoop\Loop;
+use function Ratchet\Client\connect;
 
 class ConnectCommand extends Command
 {
-    protected $signature = 'connect';
-
-    protected $description = 'Command description';
+    protected $signature = 'connect {--url=ws://127.0.0.1:8081}';
+    protected $description = 'Connect to the control WebSocket server';
 
     public function handle()
     {
-        $this->info('Connecting...');
+        $url = (string) $this->option('url');
+        $this->info('Connecting to ' . $url);
+
+        connect($url)->then(function (WebSocket $conn) {
+            $this->info("Connected!\n");
+            $conn->send(json_encode([
+                'event' => 'ping',
+            ]));
+
+            $conn->on('message', function ($msg) use ($conn) {
+                $this->info("Received: {$msg}\n");
+                $conn->close();
+            });
+
+            $conn->on('close', function ($code) use ($conn) {
+                $this->info("Connection closed ({$code})\n");
+                Loop::stop();
+            });
+
+        });
+
+        function(\Exception $e) {
+            $this->info("An error occurred: {$e->getMessage()}\n");
+            Loop::stop();
+        };
+
+        Loop::run();
+        return 0;
     }
 }
